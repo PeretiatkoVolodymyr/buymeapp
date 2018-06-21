166d721b771b9c26dc49526cd86900bfe70a9506
vperetyatko <voc@ukr.net>
2018-06-21 11:29:37 +0300

    added git ignore

--- backend/src/main/java/model/BaseEntity.java
+++ backend/src/main/java/model/BaseEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -18,5 +19,19 @@ public abstract class BaseEntity {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
     }
 }
