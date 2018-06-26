067a2c6a337b66c28e9502abbc535608ef667758
vperetyatko <voc@ukr.net>
2018-06-26 15:58:39 +0300

    added doc for Dao interface

--- backend/src/main/java/dao/Dao.java
+++ backend/src/main/java/dao/Dao.java
@@ -2,12 +2,74 @@ package dao;
 
 import java.util.List;
 
-public interface Dao<T,ID> {
+/**
+ * General DAO interface.
+ *
+ * @param <V> type of stored entity (VALUE).
+ * @param <ID> type of id (KEY) field of stored entity.
+ *
+ * @author Peretiatko Volodymyr
+ * @version 0.1
+ */
+public interface Dao<V,ID> {
 
-    T create(T entity);
-    List<T> findAll();
-    List<T> findAll(int offset, int length);
-    T find(ID id);
-    T remove(ID id);
-    T update(T entity);
+    /**
+     * Creates new entity instance (stores it to DB).
+     *
+     * @param entity new instance, which will be stored.
+     * @return stored variant of instance (with assigned ID field), if operation
+     * was successful, or null, if it wasn't.
+     */
+    V create(V entity);
+
+    /**
+     * Searches for all stored entity instances.
+     *
+     * @return List of stored instances, if operation was successful, or null,
+     * if it wasn't.
+     */
+    List<V> findAll();
+
+    /**
+     * Searches for all stored entity instances.
+     * Shows limited result of search.
+     *
+     * @param offset start position for limited result List (all elements
+     *               "before" offset will be not included into result List).
+     * @param length number of elements after offset position (including),
+     *               which will be included into result List.
+     * @return limited List of stored instances, if operation was successful, or null,
+     * if it wasn't.
+     */
+    List<V> findAll(int offset, int length);
+
+    /**
+     * Searches for stored entity instance by its ID.
+     *
+     * @param id target instance's ID (key).
+     * @return needed instance, if operation was successful, or null,
+     * if it wasn't or if such instance was not stored to DB.
+     */
+    V find(ID id);
+
+    /**
+     * Removes from DB stored entity instance by its ID.
+     *
+     * @param id target instance's ID (key).
+     * @return removed instance, if operation was successful, or null,
+     * if it wasn't or if such instance was not stored to DB.
+     */
+    V remove(ID id);
+
+    /**
+     * Updates stored entity instance.
+     * Note: it will not create new instance (will return null instead),
+     * it can only update existent one!
+     *
+     * @param entity new variant of stored instance.
+     * @return old variant of instance, if operation was successful, or null,
+     * if it wasn't or if such instance was not stored to DB.
+     */
+    V update(V entity);
+
 }
