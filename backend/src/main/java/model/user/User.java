6013a08416e3d3d81bbecb8dc4d3cd676aff56ec
vperetyatko <voc@ukr.net>
2018-06-26 16:40:33 +0300

    added description User

--- backend/src/main/java/model/user/User.java
+++ backend/src/main/java/model/user/User.java
@@ -3,24 +3,38 @@ package model.user;
 import model.BaseEntity;
 import model.user.Adress;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+/**
+ * User POJO model.
+ *
+ * @author PeretiatkoVolodymyr
+ * @version 0.1
+ *
+ * @see BaseEntity
+ */
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
 
+    @Column
     private String login;
+
+    @Column
     private String password;
+
+    @Column
     private String name;
+
+    @Column
     private String email;
+
     @OneToOne
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Adress address;
-    private String phone;
 
+    @Column
+    private String phone;
 
 
 
