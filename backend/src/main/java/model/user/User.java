02cee341f91b2663909b12782ca49bfeb5c23ad7
PeretiatkoVolodymyr <voc@ukr.net>
2018-06-27 09:48:00 +0300

    added User

--- backend/src/main/java/model/user/User.java
+++ backend/src/main/java/model/user/User.java
@@ -1,7 +1,7 @@
 package model.user;
 
 import model.BaseEntity;
-import model.user.Adress;
+
 
 import javax.persistence.*;
 
@@ -36,6 +36,63 @@ public class User extends BaseEntity {
     @Column
     private String phone;
 
+    public User() {
+    }
 
+    public User(String login, String password, String name, String email, Adress address, String phone) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Adress getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(Adress address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
