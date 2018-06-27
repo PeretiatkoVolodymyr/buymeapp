7a66b30e6b18256046dfaa3b5a501019cab3a987
PeretiatkoVolodymyr <voc@ukr.net>
2018-06-27 19:17:17 +0300

    added OrderLine

--- backend/src/main/java/model/order/OrderLine.java
+++ backend/src/main/java/model/order/OrderLine.java
@@ -1,18 +1,28 @@
 package model.order;
 
+import model.BaseEntity;
 import model.product.Product;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
-public class OrderLine {
+public class OrderLine extends BaseEntity {
 
     @ManyToOne
+    @Column
     private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
+
+    @Column
     private int price;
+
+    @Column
     private int quantity;
+
+    @Column
     private int amount;
 
 }
