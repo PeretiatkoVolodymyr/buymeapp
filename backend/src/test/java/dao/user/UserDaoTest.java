3d31309979945c11c7d218999373e00362242f08
PeretiatkoVolodymyr <voc@ukr.net>
2018-06-27 19:10:30 +0300

    added Tests UserDao

---
+++ backend/src/test/java/dao/user/UserDaoTest.java
@@ -0,0 +1,42 @@
+package dao.user;
+
+import model.user.User;
+import org.junit.*;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.lang.reflect.Field;
+
+public class UserDaoTest {
+
+    private static UserDaoImpl dao;
+    private static EntityManagerFactory factory;
+
+    @BeforeClass
+    public static void init() throws Exception {
+
+        //init factory and dao
+        factory = Persistence.createEntityManagerFactory("hibernate-test-unit");
+        dao = new UserDaoImpl();
+
+        //set factory in dao
+        Field fieldFactory = dao.getClass().getField("factory");
+        fieldFactory.set(dao, factory);
+
+    }
+
+    @AfterClass
+    public static void down() throws Exception {
+        factory.close();
+        dao = null;
+    }
+
+    @Test
+    public void create() throws Exception {
+
+        User newEntity = dao.create(new User());
+        Assert.assertTrue(newEntity.getId() != 0);
+    }
+
+
+}
